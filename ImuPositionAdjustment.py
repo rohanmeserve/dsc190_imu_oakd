b80306fdc57f8074fcd97670fc32f1e3b839bdc6
rohan <rmeserve@ucsd.edu>
2024-03-14 16:14:59 -0700

    adjusted update and run_threaded functions

--- ImuPositionAdjustment.py
+++ ImuPositionAdjustment.py
@@ -20,7 +20,7 @@ class PositionEstimator:
     '''
 
     def __init__(self):
-        # position and velocity values
+        # position, velocity, and orientation values
         self.x = 0.
         self.y = 0.
         self.vx = 0.
@@ -32,41 +32,53 @@ class PositionEstimator:
         self.last_pos_y = 0.
 
         # timestamps for calculating time difference
-        self.last_time = time.time()
+        self.last_run_time = time.time()
+        self.last_update_time = time.time()
         self.last_gps_time = time.time()
 
-        # create file for 
+        self.start_time = time.time()
+
+        # create file for analysis purposes
         self.file = open('estimations.csv', 'w')
+        # write column headers
         self.file.write('(est_x, est_y), (act_x, act_y), timestamp\n')
 
     def update(self):
-        # runs as frequently as possible; update stored position using acceleration (rotation offset compensated by orientation), stored velocity, and time difference
+        # runs as frequently as possible; update stored position using stored velocity and time difference
 
         # update current time to find time difference since last update
         curr_time = time.time()
-        dt = curr_time - self.last_time
-        
-        # update stored orientation
-        self.yaw += float(gyr_x) * dt
-
-        # rotate received acceleration vectors
-        ax = float(acl_x) * math.cos(-self.yaw) - float(acl_y) * math.sin(-self.yaw)
-        ay = float(acl_x) * math.sin(-self.yaw) - float(acl_y) * math.cos(-self.yaw)
-
-        # update stored velocity
-        self.vx += ax * dt
-        self.vy += ay * dt
+        dt = curr_time - self.last_update_time
 
         # update stored position
         self.x += self.vx * dt
         self.y += self.vy * dt
 
         # update time for next update
-        self.last_time = curr_time
+        self.last_update_time = curr_time
 
 
 
     def run_threaded(self, acl_x: float, acl_y: float, gyr_x: float, pos_x: float, pos_y: float):
+        # updates stored velocity and orientation using accelerometer and gyroscope readings
+
+        curr_time = time.time()
+        dt = curr_time - self.last_run_time
+
+        # update orientation
+        self.yaw += float(gyr_x) * dt
+
+        # rotate accerlation vectors
+        ax = float(acl_x) * math.cos(-self.yaw) - float(acl_y) * math.sin(-self.yaw)
+        ay = float(acl_x) * math.sin(-self.yaw) - float(acl_y) * math.cos(-self.yaw)
+
+        # update velocity
+        self.vx += ax * dt
+        self.vy += ay * dt
+
+        # update time for next run
+        self.last_run_time = curr_time
+
         # checks if the pos/x and pos/y from gps have recently been updated
         # if true, reset stored position and velocity to match actual values
         if pos_x != self.last_pos_x or pos_y != self.last_pos_y:
@@ -90,66 +102,22 @@ class PositionEstimator:
             # update last gps time to current
             self.last_gps_time = curr_time
 
+        # updates last known gps position to current gps position
         self.last_pos_x = pos_x
         self.last_pos_y = pos_y
+
+        # write predicted pos and last known true pos to file
+        self.file.write(f'({self.x},{self.y}), ({pos_x}, {pos_y}, {self.start_time - self.curr_time})\n')
 
         return self.x, self.y, self.yaw
 
 
     def run(self, acl_x: float, acl_y: float, gyr_x: float, pos_x: float, pos_y: float):
-        # essentially a combination of update -> run_threaded, for testing purposes in __main__
+        # essentially a combination of update and run_threaded, for testing purposes in __main__
 
-        curr_time = time.time()
-        dt = curr_time - self.last_time
+        self.update()
+        self.run_threaded(acl_x, acl_y, gyr_x, pos_x, pos_y)
 
-        # update orientation
-        self.yaw += float(gyr_x) * dt
-
-        # rotate accerlation vectors
-        ax = float(acl_x) * math.cos(-self.yaw) - float(acl_y) * math.sin(-self.yaw)
-        ay = float(acl_x) * math.sin(-self.yaw) - float(acl_y) * math.cos(-self.yaw)
-
-        # update velocity
-        self.vx += ax * dt
-        self.vy += ay * dt
-
-        # update position
-        self.x += self.vx * dt
-        self.y += self.vy * dt
-
-
-        self.last_time = curr_time
-
-        # if the gps has been updated, reset position and velocity to its "true" values
-        if pos_x != self.last_pos_x or pos_y != self.last_pos_y:
-            print('resetting with gps instruciton')
-            # calculate time difference since last gps update
-            gps_dt = curr_time - self.last_gps_time
-            # reset position based on gps values
-            pos_x = float(pos_x)
-            pos_y = float(pos_y)
-            self.x = pos_x
-            self.y = pos_y
-            self.z = 0
-            # reset velocity based on gps delta
-            self.vx = (self.last_pos_x - pos_x) / gps_dt
-            self.vy = (self.last_pos_y - pos_y) / gps_dt
-            self.vz = 0
-            # reset yaw
-            a_mag = math.sqrt((self.last_pos_x - pos_x)**2 + (self.last_pos_y - pos_y)**2)
-            b_mag = 1
-            self.yaw = math.acos((self.last_pos_y - pos_y) / (a_mag * b_mag))
-            # update last gps time to current
-            self.last_gps_time = curr_time
-
-        self.last_pos_x = pos_x
-        self.last_pos_y = pos_y
-
-        # log estimation
-        logging.info(f"Estimated: ({self.x},{self.y},{self.yaw})")
-
-        # write predicted pos and last known true pos to file for later analysis
-        self.file.write(f'({self.x},{self.y}), ({pos_x}, {pos_y}, {self.curr_time})\n')
         return self.x, self.y, self.yaw
 
 if __name__ == "__main__":
